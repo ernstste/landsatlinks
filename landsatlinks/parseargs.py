b3d3184f42993a3be10d6b2eca4f1d77b774794c
Stefan Ernst <15325433+ernstste@users.noreply.github.com>
2022-10-02 12:52:56 +0200

    replace prlist with aoi, more concise help

--- landsatlinks/parseargs.py
+++ landsatlinks/parseargs.py
@@ -1,5 +1,6 @@
 import argparse
 from datetime import date
+
 from landsatlinks import __version__
 
 
@@ -8,10 +9,10 @@ def parse_cli_arguments():
     currentDate = date.today().strftime('%Y-%m-%d')
 
     parser = argparse.ArgumentParser(
-        description='Create download URLs for Landsat Collection 2 Level 1 data using the USGS machine-to-machine API.'
-                    'Try "landsatlinks search" to search for (and download) Landsat C1 Level 1 product bundles or '
+        description='Create download URLs for Landsat Collection 2 Level 1 data using the USGS machine-to-machine API. '
+                    'Use "landsatlinks search" to search for (and download) Landsat C1 Level 1 product bundles or '
                     '"landsatlinks download" to only download product bundles from a prior search. '
-                    'Type "landsatlinks <command> --help" for more information on a specific command. '
+                    'Type "landsatlinks <command> --help" for more information. '
                     'https://github.com/ernstste/landsatlinks'
     )
     parser.add_argument(
@@ -27,9 +28,12 @@ def parse_cli_arguments():
         'search',
         help='Search for products, generate download links and optionally download the results.'
     )
+    search_group = parser_search.add_mutually_exclusive_group()
     parser_search.add_argument(
-        'pathrowlist',
-        help='Path to text file containing allowed path/rows, one per line. Format: PPPRRR (keep padding zeroes!).'
+        'aoi',
+        help='Path to AOI file. '
+             'Supported formats: .txt (one path/row per line in the format PPPRRR) or '
+             '.shp, .geojson, .gpkg (vector files).'
     )
     parser_search.add_argument(
         'output_dir',
@@ -46,8 +50,8 @@ def parse_cli_arguments():
     parser_search.add_argument(
         '-d', '--daterange',
         default=f'1970-01-01,{currentDate}',
-        help='Start date and end date = date range to be considered. Format: YYYY-MM-DD,YYYY-MM-DD.\n'
-             'Default: full archive until today.'
+        help='Start date and end date. Format: YYYY-MM-DD,YYYY-MM-DD.\n'
+             'Default: beginning of archive until today.'
     )
     parser_search.add_argument(
         '-c', '--cloudcover',
@@ -57,7 +61,7 @@ def parse_cli_arguments():
     parser_search.add_argument(
         '-m', '--months',
         default='1,2,3,4,5,6,7,8,9,10,11,12',
-        help='Seasonal filter: define the months to be considered. \nDefault: 1,2,3,4,5,6,7,8,9,10,11,12'
+        help='Seasonal filter - months to be considered. \nDefault: 1,2,3,4,5,6,7,8,9,10,11,12'
     )
     parser_search.add_argument(
         '-t', '--tier',
@@ -72,26 +76,26 @@ def parse_cli_arguments():
         help='Landsat level of processing. Valid levels: L1TP, L1GT, L1GS'
     )
 
-    parser_search.add_argument(
+    search_group.add_argument(
         '--download',
         action='store_true',
         help='Download the product bundles directly after creating the download links.'
     )
-    parser_search.add_argument(
+    search_group.add_argument(
         '-n', '--no-action',
         action='store_true',
-        help='Only search for product bundles matching search criteria and print the total data volume. No download '
-             'links will be created and no products will not be downloaded.'
+        help='Only search for product bundles and print info without generating links or downloading.'
     )
     parser_search.add_argument(
         '-f', '--forcelogs',
-        help='Path to FORCE Level-2 log files. Search this folder for log files and only consider products that '
-             'have not been processed before.'
+        help='Path to FORCE Level-2 log files. Will skip products that have been processed by FORCE.'
     )
     parser_search.add_argument(
         '--secret',
-        help='Path to a file containing the username and password for the USGS EarthExplorer.'
+        help='Path to the file containing the username and password for M2MApi access (EarthExplorer login).'
     )
+
+
 
     # Download parser arguments
     parser_dl = subparsers.add_parser(
@@ -104,7 +108,7 @@ def parse_cli_arguments():
     )
     parser_dl.add_argument(
         'output_dir',
-        help='Path to the output directory where the download links and downloaded products will be stored.'
+        help='Path to the output directory where the downloaded products will be stored.'
     )
 
     return parser.parse_args()
