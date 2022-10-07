5abfdcf69d6b61e7b58c1d19f45c66abc1e71eef
Stefan Ernst <15325433+ernstste@users.noreply.github.com>
2022-10-07 16:35:28 +0200

    get program name from sys.argv

--- landsatlinks/utils.py
+++ landsatlinks/utils.py
@@ -2,12 +2,13 @@ import os
 import platform
 import re
 import shutil
+import sys
 import time
 from math import floor, log
 from pathlib import Path
 
 PRODUCT_ID_REGEX = re.compile('(L[CET]0[45789]_L1[A-Z]{2}_[0-9]{6}_[0-9]{8}_[0-9]{8}_0[12]_T1|T2|RT)')
-PROG_NAME = 'landsatlinks'
+PROG_NAME = os.path.basename(sys.argv[0])
 
 
 def countdown(seconds: int):
